1bf1739d243731b1c5aac86fb7e3e36c6038d221
Flores-Brandon <I22050339@monclova.tecnm.mx>
2023-12-14 00:13:50 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto Final 3er Semestre  VBConsola/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+        Dim [option] As Integer
+        Dim Menu As New MenuStructures()
+        Do
+            [option] = 0
+            Menu.PrintArray(Menu._TypeDataStructures, Menu.Name)
+            Console.Write("Write the number of one option: ")
+            [option] = Menu.Option([option])
+        Loop While [option] <> 7
+    End Sub
+
+End Module
